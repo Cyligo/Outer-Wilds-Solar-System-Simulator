de2642bc0ea7191495589e1d055a6f349e27a08b
Cyligo <guilhermecarvalho15978@gmail.com>
2025-07-16 18:55:30 -0300

    Labels and Twins Changes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ZOOM_SCALE = 1e-9
 zoomed = False
 
 class Body:
-    def __init__(self, x, y, vx, vy, mass, color, radius):
+    def __init__(self, x, y, vx, vy, mass, color, radius, name):
         self.x = x
         self.y = y
         self.vx = vx
@@ -27,6 +27,7 @@ class Body:
         self.mass = mass
         self.color = color
         self.radius = radius
+        self.name = name
         self.trail = []
         
     def update_position(self, bodies):
@@ -56,32 +57,43 @@ class Body:
         if len(self.trail) > 200:
             self.trail.pop(0)
         
+    def draw_text(text, font, text_color, x, y):
+        font = pygame.font.SysFont("Arial", 30)
+        text_surface = font.render(text, True, text_color)
+        screen.blit(text_surface, (x, y))
+        
     def draw(self, screen):
         if len(self.trail) > 1:
             pygame.draw.lines(screen, (50, 50, 50), False, self.trail, 1)
-            
+
         current_scale = ZOOM_SCALE if zoomed else SCALE
-        
+
         screen_x = float(self.x * current_scale + WIDTH // 2)
         screen_y = float(self.y * current_scale + HEIGHT // 2)
         pygame.draw.circle(screen, self.color, (screen_x, screen_y), self.radius)
 
+        # Draw the name of the planet
+        font = pygame.font.SysFont("Arial", 15) if zoomed else pygame.font.SysFont("Arial", 10)
+        text_surface = font.render(self.name, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(screen_x, screen_y + self.radius + 12))
+        screen.blit(text_surface, text_rect)
+    
 bodies = [
-    Body(0,0,0,0, 1.989e30, (255,255,0), 10),           # Sun
+    Body(0,0,0,0, 1.989e30, (255,255,0), 15, "Sun"),
+
+    Body(5.79e10, 1e9, -2e3, 4.74e4, 4.302e23, (255,75,8), 5, "Ash Twin"),
     
-    Body(5.79e10,0,0,4.74e4, 4.302e23, (255,75,8), 1), # Ash Twin
+    Body(5.81e10, -1e9, 2e3, 4.74e4, 4.302e23, (250,75,8), 5, "Ember Twin"),
+
+    Body(1.5e11,0,0,2.98e4,5.972e24,(0,150,0),7, "Timber Hearth"),
     
-    Body(5.78e10,0,0,4.74e4,4.302e23,(250,75,8),1),  # Ember Twin
+    Body(2.28e11,0,0,2.41e4,6.39e23,(25,0,150),8, "Brittle Hollow"),
     
-    Body(1.5e11,0,0,2.98e4,5.972e24,(0,150,0),3),    # Timber Hearth
+    Body(3.78e11,0,0,1.91e4,7.898e23,(0,200,100),11, "Giant’s Deep"),
     
-    Body(2.28e11,0,0,2.41e4,6.39e23,(25,0,150),4),    # Brittle Hollow
+    Body(4.43e11,0,0,1.81e4,8.683e23,(85,68,50),10, "Dark Bramble"),
     
-    Body(3.78e11,0,0,1.91e4,7.898e23,(0,200,100),8),    # Giant’s Deep
-    
-    Body(4.43e11,0,0,1.81e4,8.683e23,(85,68,50),7),  # Dark Bramble
-    
-    Body(5.50e11,0,0,3.48e3,9.024e23,(50,100,255),2),   # Interloper
+    Body(5.50e11,0,0,3.48e3,4.024e23,(50,100,255),6, "Interloper"),
 ]
 
 
